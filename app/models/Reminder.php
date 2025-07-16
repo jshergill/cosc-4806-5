051e02fef74098b5e0156a889941c68f86279a29
jshergill <jshergill@algomau.ca>
2025-07-16 22:05:11 +0000

    report folder added

--- app/models/Reminder.php
+++ app/models/Reminder.php
@@ -14,27 +14,31 @@ class Reminder {
     }
     public function create_reminder($subject) {
         $db = db_connect();
-        $stmt = $db->prepare("INSERT INTO Reminders (user_id, subject, created_at) VALUES (27, :subject, NOW())");
+        $stmt = $db->prepare("INSERT INTO Reminders (user_id, subject, created_at) VALUES (:user_id, :subject, NOW())");
+         $stmt->bindValue(':user_id', $_SESSION['user_id']); 
         $stmt->bindValue(':subject', $subject);
         return $stmt->execute();
     }
     public function reminder_by_id($ID) {
         $db = db_connect();
-        $stmt = $db->prepare("SELECT * FROM Reminders WHERE ID = :ID AND user_id = 27");
+        $stmt = $db->prepare("SELECT * FROM Reminders WHERE ID = :ID AND user_id = :user_id");
+         $stmt->bindValue(':user_id', $_SESSION['user_id']); 
         $stmt->bindValue(':ID', $ID);
         $stmt->execute();
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
     public function update_reminder($ID, $subject) {
         $db = db_connect();
-        $stmt = $db->prepare("UPDATE Reminders SET subject = :subject WHERE ID = :ID AND user_id = 27");
+        $stmt = $db->prepare("UPDATE Reminders SET subject = :subject WHERE ID = :ID AND user_id = :user_id");
+         $stmt->bindValue(':user_id', $_SESSION['user_id']); 
         $stmt->bindValue(':subject', $subject);
         $stmt->bindValue(':ID', $ID);
         return $stmt->execute();
     }
     public function delete_reminder($ID) {
         $db = db_connect();
-        $stmt = $db->prepare("DELETE FROM Reminders WHERE ID = :ID AND user_id = 27");
+        $stmt = $db->prepare("DELETE FROM Reminders WHERE ID = :ID AND user_id = :user_id");
+         $stmt->bindValue(':user_id', $_SESSION['user_id']); 
         $stmt->bindValue(':ID', $ID);
         return $stmt->execute();
     }
